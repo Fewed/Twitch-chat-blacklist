70b85d3d6ea7d1dcceb698a960be185ee02fd609
Mikhail Sementsov <fewebdev@gmail.com>
2018-11-16 21:17:35 +0300

    delete -> hide

--- content/content.js
+++ content/content.js
@@ -1,31 +1,50 @@
 let blackList = [];
 
-chrome.runtime.onMessage.addListener(request => {
-  	blackList = request.message;
-//    console.log(blackList);
-  }
-);
+chrome.runtime.onMessage.addListener(request => blackList = request.message);
 
-setTimeout(() => {
-setInterval(deleteMsg, 200);
+waitPoists().then(updateChat);
 
-function deleteMsg() {
+
+function waitPoists(posts = 10, period = 1, timeout = 5) {
+  return new Promise(resolve => {
+    let cnt = 0;
+    const loop = () => {
+      if (++cnt % 60*period) requestAnimationFrame(loop);
+      else {
+        const nums = document.querySelectorAll("li.tw-full-width, div.chat-line__message");
+        (nums.length < posts && cnt < 60*timeout) ? requestAnimationFrame(loop) : resolve();
+      }
+    };
+    loop();
+  });
+}
+
+
+function updateChat(period = 0.2) {
+  let cnt = 0;
+  const loop = () => {
+    if (++cnt % 60*period) requestAnimationFrame(loop);
+    else {
+      hideMsg();
+      requestAnimationFrame(loop);
+    }
+  };
+  loop();
+}
+
+
+function hideMsg() {
   const posts = blackList.reduce((acc, cur) => [...acc, ...document.querySelectorAll(`span[data-a-user="${cur.toLowerCase()}"]`)], []);
-console.log(posts);
+
   posts.map(post => {
     let c1 = c2 = "";
     while (!(c1 || c2)) {
+      if (post === document.body) break;
       post = post.parentElement;
       c1 = post.tagName === "LI" && post.className === "tw-full-width";
       c2 = post.tagName === "DIV" && post.className === "chat-line__message";
     }
-    post.remove();
+    if (post !== document.body) post.style.display = "none";
   });
 }
-}, 5000);
-
-
-
-
-
 
--- popup/popup.js
+++ popup/popup.js
@@ -24,12 +24,11 @@ function updateText() {
 	str = str.replace(/[,;]/g, "\n")
 						.split("\n")
 						.map(item => item.trim())
-						.filter(item => item)
-						.sort((a,b) => a > b ? 1 : -1)
+						.filter(item => item);
+	str = [...new Set(str)].sort((a,b) => a > b ? 1 : -1)
 						.map(item => temp[temp.push(item)-1])
 						.join("\n");
 	ta.value = "\n" + str;
-//	console.log(temp);
 	chrome.storage.local.set({twitch: temp}, () => {});
   sendMessage(temp);
 }
@@ -40,5 +39,5 @@ function toggleButton(delay = 1) {
 	setTimeout(() => {
 		[btn, img].map(item => item.classList.toggle("hide"));
 		ta.focus();
-	}, 1000*delay);
+	}, 1e3*delay);
 }
